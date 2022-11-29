da44f8b0cec97900b0eb5a735b4e4602071630e9
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-11-29 18:55:38 +0900

    01_playwright-basics

--- 01_playwright-basics/tests/login.spec.ts
+++ 01_playwright-basics/tests/login.spec.ts
@@ -2,12 +2,11 @@ import { test, expect } from '@playwright/test';
 import { loadLoginPage } from '../helpers';
 
 test.describe('Login failed', () => {
-  test.beforeEach(() => {
-    console.log('beforeEach');
+  test.beforeEach(async ({ page }) => {
+    await loadLoginPage(page);
   });
 
   test('Login failed with empty credentials', async ({ page }) => {
-    await loadLoginPage(page);
     await page.locator('button[name="login"]').click();
 
     const usernameError = page.locator('#username_err');
@@ -19,7 +18,6 @@ test.describe('Login failed', () => {
   });
 
   test('Login failed with invalid credentials', async ({ page }) => {
-    await loadLoginPage(page);
     await page.locator('#username').fill('test');
     await page.locator('#password').fill('test');
     await page.locator('button[name="login"]').click();
@@ -36,27 +34,33 @@ test.describe('Login failed', () => {
   });
 });
 
-// test.skip でスキップできる
-// test.only で1つだけ実行できる
-test('Login success', async ({ page }, testInfo) => {
-  await loadLoginPage(page);
-  await page.locator('#username').fill('kohei');
-  await page.locator('#password').fill('11111');
-  await page.locator('button[name="login"]').click();
-
-  const table = page.locator('#task-table');
-
-  // テキストまで取得する場合は、innerText() を使う
-  const newTaskBtn = page.locator('.btn.btn-primary');
-  const newTaskBtnText = await newTaskBtn.innerText();
-
-  // 部分的にscreenshotを取って、レポートに含める
-  const screenshot = await newTaskBtn.screenshot();
-  await testInfo.attach('screenshot', {
-    body: screenshot,
-    contentType: 'image/png',
+test.describe('Login success', () => {
+  test.beforeEach(async ({ page }) => {
+    await loadLoginPage(page);
   });
 
-  expect(table).toBeTruthy();
-  expect(newTaskBtnText.trim()).toBe('新しいタスク');
+  // test.skip でスキップできる
+  // test.only で1つだけ実行できる
+  test('Login success', async ({ page }, testInfo) => {
+    await loadLoginPage(page);
+    await page.locator('#username').fill('kohei');
+    await page.locator('#password').fill('11111');
+    await page.locator('button[name="login"]').click();
+
+    const table = page.locator('#task-table');
+
+    // テキストまで取得する場合は、innerText() を使う
+    const newTaskBtn = page.locator('.btn.btn-primary');
+    const newTaskBtnText = await newTaskBtn.innerText();
+
+    // 部分的にscreenshotを取って、レポートに含める
+    const screenshot = await newTaskBtn.screenshot();
+    await testInfo.attach('screenshot', {
+      body: screenshot,
+      contentType: 'image/png',
+    });
+
+    expect(table).toBeTruthy();
+    expect(newTaskBtnText.trim()).toBe('新しいタスク');
+  });
 });
